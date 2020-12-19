06d4be806a914c2529e778ec81cbe2d5df8493a4
Martin Nicholson <recyclebing+github@gmail.com>
2020-12-19 22:15:02 +0000

    Day 14 part 2 in Ruby

--- ruby/14.rb
+++ ruby/14.rb
@@ -42,11 +42,8 @@ end
 end
 
 # part 1 - sum of registers values - 13105044880745
-p @register1.values.select{ |v| v > 0 }.reduce(&:+)
-
-
-
-
+ans = @register1.values.select{ |v| v > 0 }.reduce(&:+)
+p "p1: #{ans}"
 
 
 # Apply bitmask to a value. Unknown 'X' values end up in result
@@ -58,6 +55,8 @@ def apply_bitmask_quantumly(value, mask_str)
     mask_str.chars.each_with_index{ |c,i|
         if c == 'X'
             value_str[i] = 'X'
+        elsif c == '1'
+            value_str[i] = '1'
         end
     }
     value_str
@@ -66,15 +65,21 @@ end
 # Generates all possible values of a binary strng with unknown 'X' characters
 # @param {String} address made of '0', '1', 'X'
 # @returns {Number[]} all possible decimal values
-def permute_address(address)
-    base_val = address.gsub('X', '0')
+def combine_address(address)
+    base_val = address.gsub('X', '0').to_i(2)
     address.reverse!
-    floaters = []
+    x_indices = [] # reversed string indices of X's
     address.chars.each_index{ |i|
-        floaters.push(i) if address[i] == 'X'
+        x_indices.push(i) if address[i] == 'X'
     }
-    floaters.map!{ |f| 2**f + base_val.to_i(2) }
-    floaters.permutation
+
+    combos = (1..x_indices.length).map do |n|
+        x_indices.combination(n).map do |fls|
+            base_val + fls.reject(&:nil?).map{ |f| 2**f }.reduce(&:+)
+        end
+        .to_a
+    end
+    [base_val].concat(combos.flatten)
 end
 
 # Assign bitmasked value into a specific memory address
@@ -83,21 +88,20 @@ end
 # @param {String} bitmask (length 36)
 def assign_many_via_mask(index, value, bitmask)
     quantum_address = apply_bitmask_quantumly(index, bitmask)
-    addresses = permute_address(quantum_address)
+    addresses = combine_address(quantum_address)
     addresses.each{ |addy| @register2[addy] = value }
 end
 
 @lines.each_with_index do |line,i|
     if line.start_with? "mask"
-        p i
         @bitmask = line.chomp.split(" = ")[1]
     else
         if m = line.chomp.match(/^mem\[(\d+)\] = (\d+)$/)
             assign_many_via_mask(m[1].to_i, m[2].to_i, @bitmask)
-            p @register2.size
         end
     end
 end
 
-# part 2 - sum of registers values - 0
-p @register2.values.select{ |v| v > 0 }.reduce(&:+)
+# part 2 - sum of registers values - 3505392154485
+ans = @register2.values.select{ |v| v > 0 }.reduce(&:+)
+p "p2: #{ans}"
